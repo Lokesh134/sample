a3ce0c8667f88354cf9367b2c98ccfd60b77ec12
Lokesh134 <lokeshvarma580@gmail>
2024-03-01 15:28:34 +0530

    first commit

---
+++ src/intro/DemoClass.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		webdriver driver = new chromedriver();
+
+	}
+
+}
